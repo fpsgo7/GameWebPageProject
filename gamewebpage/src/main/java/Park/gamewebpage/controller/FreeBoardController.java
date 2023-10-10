6fff9110aa6f3f672da0a1b852f8658c4642f212
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-10-10 22:27:58 +0900

    자유 게시판 글작성을 위한 컨트롤러, 서비스 메서드 작성완료 와 테스트용 H2 DB 사용준비 완료
    FreeBoardController : 글생성 메서드 작성
    FreeBoardService : 글생성 메서드 작성
    
    application.properties : H2 데이터베이스를 사용하기 위한 준비 완료

--- gamewebpage/src/main/java/Park/gamewebpage/controller/FreeBoardController.java
+++ gamewebpage/src/main/java/Park/gamewebpage/controller/FreeBoardController.java
@@ -1,7 +1,13 @@
 package Park.gamewebpage.controller;
 
+import Park.gamewebpage.domain.FreeBoard;
+import Park.gamewebpage.dto.CreateFreeBoardDTO;
 import Park.gamewebpage.service.FreeBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,5 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FreeBoardController {
     private final FreeBoardService freeBoardService;
 
+    /**
+     * 자유 게시판 글생성 컨트롤러이다.
+     * @param createFreeBoardDTO
+     * @return HTTP 상태 코드는 생성이며
+     * HTTP body에는 FreeBoard 객체로 반환한다.
+     */
+    public ResponseEntity<FreeBoard> createFreeBoard
+            (@RequestBody CreateFreeBoardDTO createFreeBoardDTO){
+        FreeBoard freeBoard
+                = freeBoardService.createFreeBoard(createFreeBoardDTO);
 
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(freeBoard);
+    }
 }
--- gamewebpage/src/main/java/Park/gamewebpage/service/FreeBoardService.java
+++ gamewebpage/src/main/java/Park/gamewebpage/service/FreeBoardService.java
@@ -1,5 +1,7 @@
 package Park.gamewebpage.service;
 
+import Park.gamewebpage.domain.FreeBoard;
+import Park.gamewebpage.dto.CreateFreeBoardDTO;
 import Park.gamewebpage.repository.IFreeBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,16 @@ import org.springframework.stereotype.Service;
 public class FreeBoardService {
 
     private final IFreeBoardRepository iFreeBoardRepository;
+
+    /**
+     * 자유 게시판 글 객체가 생성된다.
+     * CreateFreeBoardDTO 를 받아 엔티티 객체화 시킨후
+     * FreeBoard 객체화 된 객체를 반환한다.
+     * @param createFreeBoardDTO
+     * @return FreeBoard 저장소에 저장되는 객체가 반환된다.
+     */
+    public FreeBoard createFreeBoard(CreateFreeBoardDTO createFreeBoardDTO){
+        return iFreeBoardRepository
+                .save(createFreeBoardDTO.toEntity());
+    }
 }
