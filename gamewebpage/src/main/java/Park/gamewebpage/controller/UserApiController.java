4f7bcb59db2216ce0a2b61cacad43049ca71b2b9
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-10-18 19:36:46 +0900

    로그아웃 기능 구현완료
    추가사항
    UserApiController : 로그아웃을 담당하는 메서드 추가
    freeBoardList : 로그아웃 버튼 추가

--- gamewebpage/src/main/java/Park/gamewebpage/controller/UserApiController.java
+++ gamewebpage/src/main/java/Park/gamewebpage/controller/UserApiController.java
@@ -3,17 +3,38 @@ package Park.gamewebpage.controller;
 import Park.gamewebpage.dto.freeboardDTO.user.CreateUserDTO;
 import Park.gamewebpage.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
     private final UserService userService;
 
+    /**
+     * 회원가입을 담당하는 메서드
+     */
     @PostMapping("/user")
     public String signup(CreateUserDTO createUserDTO){
         userService.createUser(createUserDTO); // 회원가입 서비스
         return "redirect:/login"; // 회원가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    /**
+     * 로그아웃을 위한 메서드이다.
+     * SecurityContextLogoutHandler 의 logout을 통하여 로그아웃하여
+     * 스프링 시큐리티를 활용한다.
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response
+        , SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
