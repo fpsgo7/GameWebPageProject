222dd1bfc848e6916a584813c825218ffc61b64b
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-10-18 22:57:07 +0900

    URL 클래스 주석 과 freeBoardList 수정
    URL : 주석들로 해당 URL경로가 어느 html 파일과 js 파일에 있는지 알려준다.
    freeBoardList : 경로 작성 방식을 수정하여 다른 html 파일들과 통일하였다.

--- gamewebpage/src/main/java/Park/gamewebpage/url/URL.java
+++ gamewebpage/src/main/java/Park/gamewebpage/url/URL.java
@@ -2,8 +2,10 @@ package Park.gamewebpage.url;
 
 /**
  * URL 경로를 저장해두는 공간으로
- * 자바 파일 이외에 저장된 경로는 변수로 입력하기 어려워서
- * 임시로 문자열로 작성하였다.
+ * 자바 파일 이외에 저장된 경로는 변수로 입력할 수 없어서
+ * 경로를 입력하였다.
+ * 해당 경로를 수정하게된다면
+ * 주석에서 나와있는 ,js, html 파일에서 도 수정해주면된다.
  */
 public class URL {
     // FreeBoardAPI
@@ -20,18 +22,36 @@ public class URL {
      */
     public final static String FREE_BOARD_VIEW
             = "/view/freeBoard";
+    /**
+     * freeBoardList.html
+     */
     public final static String FREE_BOARD_VIEW_BY_ID
             = "/view/freeBoard/{id}";
+    /**
+     * freeBoardList.html
+     */
     public final static String CREATE_FREE_BOARD_VIEW
             = "/view/createFreeBoard";
+    /**
+     * freeBoard.html
+     */
     public final static String UPDATE_FREE_BOARD_VIEW
             = "/view/updateFreeBoard";
 
     // User API
+    /**
+     * login.html
+     */
     public final static String USER_LOGIN_API_VIEW
             = "/login";
+    /**
+     * signup.html
+     */
     public final static String USER_SIGNUP_API
             = "/user";
+    /**
+     * freeBoardList.html
+     */
     public final static String USER_LOGOUT_API
             = "/logout";
     // UserView
