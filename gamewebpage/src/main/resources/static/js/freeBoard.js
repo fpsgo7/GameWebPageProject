b6754fe6c03e0651afde0a71d0c3632271869d0d
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-10-26 16:27:28 +0900

    OAuth 뷰 구성, 글쓴이 보기 추가
    추가사항
    google.png 이미지를 보기위하여 추가하였다.
    token : 토큰을 위한 자바 스크립트
    
    수정 사항
    freeBoard : 기존의 자바 스크립트 문장을 토큰 관련하여 사용하기 위해 수정
    freeBoard : 글쓴이 추가
    freeBoardList : 스크립트 파일 불러오기
    login : 로그인 방식 추가

--- gamewebpage/src/main/resources/static/js/freeBoard.js
+++ gamewebpage/src/main/resources/static/js/freeBoard.js
@@ -3,23 +3,77 @@ const createButton = document.getElementById('create-btn');
 
 if (createButton) {
     createButton.addEventListener('click', event => {
-        fetch('/api/freeBoard', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value,
-                // 회원가입 전까진 임시값 사용
-                writerId: "1",
-                writerName: "1"
-            })
-        })
-            .then(() => {
-                location.replace('/view/freeBoard');
-            });
+      body = JSON.stringify({
+        title: document.getElementById('title').value,
+        content: document.getElementById('content').value,
+      });
+      function success() {
+        alert('등록 완료되었습니다.');
+        location.replace('/view/freeBoard');
+      }
+      function fail() {
+        alert('등록 실패하였습니다..');
+        location.replace('/view/freeBoard');
+      }
+      httpRequest('POST','/api/freeBoard',body,success,fail)
     });
+}
+
+// HTTP 요청을 보내는 함수
+function httpRequest(method,url,body,success,fail){
+  fetch(url,{
+    method: method,
+    headers: {
+      // 로컬 스토리지에서 액세스 토큰 값을 헤더에 추가한다.
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+      'Content-Type':  'application/json',
+    },
+    body: body
+  }).then(response => {
+    if(response.status === 200 || response.status === 201 ){
+      return success();
+    }
+    const refresh_token = getCookie('refresh_token');
+    if(response.status === 401 && refresh_token){
+      fetch('/api/token',{
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          refreshToken: getCookie('refresh_token'),
+        }),
+      }).then(res => {
+        if(res.ok){
+          return res.json();
+        }
+      }).then(result =>{
+        // 재발급이 성공하면 로컬 스토리지 값을 새로운 액세스 토큰으로 한다.
+        localStorage.setItem('access_token', result.accessToken);
+        httpRequest(method,url,body,success,fail);
+      }).catch(error => fail());
+    }else {
+      return fail();
+    }
+  });
+}
+
+// 쿠키를가져오는 함수
+function getCookie(key){
+  var result = null;
+  var cookie = document.cookie.split(';');
+  cookie.some(function(item){
+    item = item.replace(' ','');
+
+    var dic = item.split('=');
+
+    if(key === dic[0]){
+      result = dic[1];
+      return true;
+    }
+  });
+  return result;
 }
 
 // 수정 기능
@@ -31,19 +85,22 @@ if (modifyButton) {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
-        fetch('/api/freeBoard/'+id, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
+        body = JSON.stringify({
+          title: document.getElementById('title').value,
+          content: document.getElementById('content').value
         })
-            .then(() => {
-                location.replace('/view/freeBoard/'+id);
-            });
+
+        function success() {
+          alter('수정 완료되었습니다.');
+          location.replace('/view/freeBoard/'+id)
+        }
+
+        function fail() {
+          alter('수정 실패하였습니다.');
+          location.replace('/view/freeBoard/'+id)
+        }
+
+        httpRequest('PUT','/api/freeboard/'+id,body,success,fail);
     });
 }
 
@@ -53,11 +110,16 @@ const deleteButton = document.getElementById('delete-btn');
 if(deleteButton){
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('freeBoard-id').value;
-        fetch('/api/freeBoard/'+id,{
-            method: 'DELETE'
-        })
-        .then(() => {
-            location.replace('/view/freeBoard');
-        });
+        function success() {
+          alter('삭제 완료되었습니다.');
+          location.replace('/view/freeBoard')
+        }
+
+        function fail() {
+          alter('삭제 실패하였습니다.');
+          location.replace('/view/freeBoard')
+        }
+
+        httpRequest('DELETE','/api/freeBoard/'+id,null,success,fail);
     });
 }
