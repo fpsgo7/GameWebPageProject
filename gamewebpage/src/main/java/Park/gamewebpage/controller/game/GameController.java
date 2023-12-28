5bd89ce7d83b08fd8a7181230b8359b77cc27791
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-12-28 20:24:28 +0900

    유니티의 HTTP 요청 처리하기 : 로그인 요청 처리하기 완료
    유니티가 로그인씨 요청하는 http패킷을 받아 작업을 처리해주는 매서드를 추가하였다.
    login 메서드 : http body에 이메일 값과 비밀번호 값을 받아
    아이디와 비밀번호가 올바른지 확인한후 결과에따라 값을 다르게 json을 형식으로 반환해준다.

--- gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
+++ gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
@@ -1,8 +1,14 @@
 package Park.gamewebpage.controller.game;
 
+import Park.gamewebpage.dto.Character.GetGameCharacterDTO;
+import Park.gamewebpage.dto.user.GetUserDTO;
+import Park.gamewebpage.service.GameCharacterService;
+import Park.gamewebpage.service.UserService;
+import Park.gamewebpage.url.URL;
 import lombok.RequiredArgsConstructor;
 import net.minidev.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,7 +16,8 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 @RestController
 public class GameController {
-
+    private final UserService userService;
+    private final GameCharacterService gameCharacterService;
     JSONObject jsonObject = new JSONObject();
 
     /**
@@ -22,7 +29,8 @@ public class GameController {
     public ResponseEntity<JSONObject> getTest(){
 
         System.out.println("get 요청이 왔습니다.");
-        jsonPut("true");
+        jsonObject.clear();
+        jsonObject.put("true","ture");
         return ResponseEntity
                 .ok()
                 .body(jsonObject);
@@ -37,18 +45,66 @@ public class GameController {
             @RequestBody HashMap<String, Object> map
     ){
         System.out.println(map);
-        jsonPut("true");
+        jsonObject.clear();
+        jsonObject.put("true","ture");
         return ResponseEntity
                 .ok()
                 .body(jsonObject);
     }
 
     /**
-     * json 오브젝트를 초기화한 후
-     * 인자 값을 받아 Json에 값을 넣어준다.
+     * 유니티 에서의 로그인
+     * @param loginInfo
+     * @return
      */
-    private void jsonPut(String isSuccess){
+    @PostMapping("/game/user")
+    public ResponseEntity<JSONObject> login(
+            @RequestBody HashMap<String, Object> loginInfo
+    ){
         jsonObject.clear();
-        jsonObject.put("isSuccess",isSuccess);
+        System.out.println("입력된 이메일값"+loginInfo.get("email").toString());
+        System.out.println("입력된 비밀번호 값"+loginInfo.get("password").toString());
+        GetUserDTO user = null;
+        try{
+            user = new GetUserDTO(userService.findByEmail(loginInfo.get("email").toString()));
+        }catch (Exception e){
+            // 아이디가 잘못됬을 경우
+            jsonObject.put("isUser", "false");
+            return ResponseEntity
+                    .ok()
+                    .body(jsonObject);
+        }
+
+        // 비밀전호는 BCryptPasswordEncoder 로 암호화 되있기 때문에 encoder.matches가 필요하다.
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        // 비밀번호 확인
+        if(encoder.matches(loginInfo.get("password").toString(),user.getPassword())){
+            // 로그인이 성공한다면!
+            jsonObject.put("isUser","true");
+            inputUserInfoJson(user.getId(),user.getEmail(),user.getNickname());
+            inputGameCharacterInfoJson(user.getEmail());
+        }else {
+            jsonObject.put("isUser", "false");
+        }
+
+
+        return ResponseEntity
+                .ok()
+                .body(jsonObject);
+    }
+    private void inputUserInfoJson(Long userId,String email, String userNickname ){
+        jsonObject.put("userEmail",email);
+        jsonObject.put("userNickname",userNickname);
+    }
+    private  void inputGameCharacterInfoJson(String email){
+        GetGameCharacterDTO gameCharacter;
+        try{
+            gameCharacter = new GetGameCharacterDTO(gameCharacterService.findByEmail(email));
+            jsonObject.put("isGameCharacter","true");
+            jsonObject.put("gameCharacterNickname",gameCharacter.getNickname());
+            jsonObject.put("gameCharacterHighScore", gameCharacter.getHighScore());
+        }catch (Exception e){
+            jsonObject.put("isGameCharacter","false");
+        }
     }
 }
