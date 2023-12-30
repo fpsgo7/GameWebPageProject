e1db21a9260c9c7f0756846b6f61dbcf846f30bc
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-12-30 12:30:20 +0900

    유니티의 HTTP 요청 처리하기 : 해당 컨트롤러 수정하기
    http 연결관련 작업이 끝났으니 코드를 리팩토링 하였다.

--- gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
+++ gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
@@ -29,38 +29,6 @@ public class GameController {
     JSONObject jsonObject = new JSONObject();
 
     /**
-     * 유니티 에서의 http test를 위한
-     * 컨트롤러
-     * @return
-     */
-    @GetMapping("/game/test")
-    public ResponseEntity<JSONObject> getTest(){
-
-        System.out.println("get 요청이 왔습니다.");
-        jsonObject.clear();
-        jsonObject.put("true","ture");
-        return ResponseEntity
-                .ok()
-                .body(jsonObject);
-    }
-
-    /**
-     * 유니티 에서의 http post
-     * test를 위한 컨트롤러
-     */
-    @PostMapping("/game/test")
-    public ResponseEntity<JSONObject> postTest(
-            @RequestBody HashMap<String, Object> map
-    ){
-        System.out.println(map);
-        jsonObject.clear();
-        jsonObject.put("true","ture");
-        return ResponseEntity
-                .ok()
-                .body(jsonObject);
-    }
-
-    /**
      * 유니티 에서의 로그인
      * @param loginInfo
      * @return
@@ -70,11 +38,11 @@ public class GameController {
             @RequestBody HashMap<String, Object> loginInfo
     ){
         jsonObject.clear();
-        //System.out.println("입력된 이메일값"+loginInfo.get("email").toString());
-        //System.out.println("입력된 비밀번호 값"+loginInfo.get("password").toString());
+        String email = loginInfo.get("email").toString();
+        String password = loginInfo.get("password").toString();
         GetUserDTO user = null;
         try{
-            user = new GetUserDTO(userService.findByEmail(loginInfo.get("email").toString()));
+            user = new GetUserDTO(userService.findByEmail(email));
         }catch (Exception e){
             // 아이디가 잘못됬을 경우
             jsonObject.put("isUser", "false");
@@ -86,7 +54,7 @@ public class GameController {
         // 비밀전호는 BCryptPasswordEncoder 로 암호화 되있기 때문에 encoder.matches가 필요하다.
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         // 비밀번호 확인
-        if(encoder.matches(loginInfo.get("password").toString(),user.getPassword())){
+        if(encoder.matches(password,user.getPassword())){
             // 로그인이 성공한다면!
             jsonObject.put("isUser","true");
             inputUserInfoJson(user.getId(),user.getEmail(),user.getNickname());
@@ -94,7 +62,6 @@ public class GameController {
         }else {
             jsonObject.put("isUser", "false");
         }
-
 
         return ResponseEntity
                 .ok()
@@ -126,19 +93,17 @@ public class GameController {
             @RequestBody HashMap<String, Object> gameCharacterInfo
     ){
         jsonObject.clear();
-        System.out.println("입력된 이메일값"+gameCharacterInfo.get("email").toString());
-        System.out.println("입력된 닉네임 값"+gameCharacterInfo.get("nickname").toString());
+        String email = gameCharacterInfo.get("email").toString();
+        String nickname = gameCharacterInfo.get("nickname").toString();
 
         try{
             GetGameCharacterDTO gameCharacter
                     =new GetGameCharacterDTO(gameCharacterService
-                    .createGameCharacter(gameCharacterInfo.get("email").toString(),
-                            gameCharacterInfo.get("nickname").toString(),
-                            0L));
+                    .createGameCharacter(email, nickname, 0L));
             gameHighScoreService.createGameHighScore(
                     GameHighScore.builder()
-                            .email(gameCharacterInfo.get("email").toString())
-                            .gameCharacterNickname(gameCharacterInfo.get("nickname").toString())
+                            .email(email)
+                            .gameCharacterNickname(nickname)
                             .highScore(0L)
                             .lastedTime(LocalDateTime.now())
                             .build()
@@ -161,7 +126,7 @@ public class GameController {
      * 게임 랭크 전체 정보를 가져와 반환한다.
      * @return
      */
-    @GetMapping("/game/gameHighScore")
+    @GetMapping("/game/gameHighScores")
     public ResponseEntity<List<GameHighScore>> getGameCharacterRankList(){
         Sort sort = Sort.by(
                 Sort.Order.desc("highScore"),
