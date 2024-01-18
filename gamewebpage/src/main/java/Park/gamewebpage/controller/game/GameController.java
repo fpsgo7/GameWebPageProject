48675f8a8673096680dfd905e5d1bf9923bc987a
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-01-18 22:37:10 +0900

    유니티의 HTTP 요청 처리하기 : http 요청에 보안 요소 추가하기
    유니티에서 로그인 하면 암호화 값을 유니티에 보내주고 유니티는 요청할때마다 암호화된 값을 보내주어 해당 값을 복호화 하여 재대로된 요청인지 확인할 수 있게 되었다
    
    GameController.java : UNITY_AUTHENTICATION 은 암호화 하기전 값으로 키로 사용된다.
    유니티는 해당 키의 암호화된 값을 받아 http 요청마다 해당 값을 보내 재대로된 요청임을 인증한다
    
    UnityAuthentication 메서드에서 유니티의 요청으로 보낸 파라미터 값의 암호화된 값을 matches를 통해 판단하여 정상적인 요청임을 확인한다.
    
    이방식으로 임의로 http 요청만을 보내어 잘못된 조작을 막을 수 있다.

--- gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
+++ gamewebpage/src/main/java/Park/gamewebpage/controller/game/GameController.java
@@ -11,11 +11,9 @@ import net.minidev.json.JSONObject;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -23,9 +21,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class GameController {
+    // http 주소가 유출될경우 아무나 주소로 요청할 수 있기 때문에
+    // 로그인할 때 유니티가 웹서버로부터 인증용 비번을 받고
+    // 유니티가 요청할때 인증용비번을 같이 보내며 요청한다.
+    private final String UNITY_AUTHENTICATION = "a8r5h4m2vc3sd5y4t5k3gh21vg";
+
     private final UserService userService;
     private final GameCharacterService gameCharacterService;
     private final GameHighScoreService gameHighScoreService;
+
+    // 암호화와 복호화를 위해 사용한다.
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     JSONObject jsonObject = new JSONObject();
 
     /**
@@ -43,21 +49,20 @@ public class GameController {
         GetUserDTO user = null;
         try{
             user = new GetUserDTO(userService.findByEmail(email));
-        }catch (Exception e){
+        }catch (Exception e) {
             // 아이디가 잘못됬을 경우
             jsonObject.put("isUser", "false");
             return ResponseEntity
                     .ok()
                     .body(jsonObject);
         }
-
         // 비밀전호는 BCryptPasswordEncoder 로 암호화 되있기 때문에 encoder.matches가 필요하다.
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         // 비밀번호 확인
         if(encoder.matches(password,user.getPassword())){
             // 로그인이 성공한다면!
             jsonObject.put("isUser","true");
-            inputUserInfoJson(user.getId(),user.getEmail(),user.getNickname());
+            jsonObject.put("UNITY_AUTHENTICATION",encoder.encode(UNITY_AUTHENTICATION));// 유니티 인증 비번 보내기
+            inputUserInfoJson(user.getEmail(),user.getNickname());
             inputGameCharacterInfoJson(user.getEmail());
         }else {
             jsonObject.put("isUser", "false");
@@ -67,7 +72,7 @@ public class GameController {
                 .ok()
                 .body(jsonObject);
     }
-    private void inputUserInfoJson(Long userId,String email, String userNickname ){
+    private void inputUserInfoJson(String email, String userNickname ){
         jsonObject.put("userEmail",email);
         jsonObject.put("userNickname",userNickname);
     }
@@ -90,12 +95,16 @@ public class GameController {
      */
     @PostMapping("/game/gameCharacter")
     public ResponseEntity<JSONObject> createGameCharacter(
-            @RequestBody HashMap<String, Object> gameCharacterInfo
+            @RequestBody HashMap<String, Object> gameCharacterInfo,
+            HttpServletRequest request
     ){
         jsonObject.clear();
         String email = gameCharacterInfo.get("email").toString();
         String nickname = gameCharacterInfo.get("nickname").toString();
 
+        if(!UnityAuthentication(request.getParameter("unityAuthenticaiton").toString()))
+            return ResponseEntity.ok().body(null);
+           
         try{
             GetGameCharacterDTO gameCharacter
                     =new GetGameCharacterDTO(gameCharacterService
@@ -127,7 +136,13 @@ public class GameController {
      * @return
      */
     @GetMapping("/game/gameHighScores")
-    public ResponseEntity<List<GameHighScore>> getGameCharacterRankList(){
+    public ResponseEntity<List<GameHighScore>> getGameCharacterRankList(
+            HttpServletRequest request
+    ){
+
+        if(!UnityAuthentication(request.getParameter("unityAuthenticaiton").toString()))
+            return ResponseEntity.ok().body(null);
+
         Sort sort = Sort.by(
                 Sort.Order.desc("highScore"),
                 Sort.Order.asc("lastedTime")    );
@@ -146,8 +161,12 @@ public class GameController {
      */
     @PostMapping("/game/gameHighScore")
     public ResponseEntity<GameHighScore>  setGameHighScore(
-            @RequestBody HashMap<String, Object> gameHighScoreInfo
+            @RequestBody HashMap<String, Object> gameHighScoreInfo,
+            HttpServletRequest request
     ){
+        if(!UnityAuthentication(request.getParameter("unityAuthenticaiton").toString()))
+            return ResponseEntity.ok().body(null);
+
         GameHighScore gameHighScore = null;
         String email = gameHighScoreInfo.get("email").toString();
         String newScore = gameHighScoreInfo.get("newScore").toString();
@@ -163,5 +182,13 @@ public class GameController {
                 .ok()
                 // 자동으로 json 방식으로 보내준다.
                 .body(gameHighScore);
+    }
+
+    public boolean UnityAuthentication(String unityAuthentication){
+        // 게임이 실행중인 유니티에서 요청이 온것인지 확인하기
+        if(encoder.matches(UNITY_AUTHENTICATION,unityAuthentication)){
+            return true;
+        }
+        return  false;
     }
 }
