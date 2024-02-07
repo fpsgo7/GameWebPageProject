0a60aa507e0043ce655f0cbeaa73729e309e9132
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-02-07 17:51:47 +0900

    Revert "AWS 호스팅 해보기" 테스트 파트만 되돌리기
    This reverts commit 0973b84a1076c91e097ff6c0874c616577869fd0.

---
+++ gamewebpage/src/test/java/Park/gamewebpage/controller/FreeBoardCommentApiControllerTest.java
@@ -0,0 +1,141 @@
+package Park.gamewebpage.controller;
+
+import Park.gamewebpage.domain.FreeBoard;
+import Park.gamewebpage.domain.FreeBoardComment;
+import Park.gamewebpage.domain.User;
+import Park.gamewebpage.dto.freeboardcomment.CreateFreeBoardCommentDTO;
+import Park.gamewebpage.repository.IFreeBoardCommentRepository;
+import Park.gamewebpage.repository.IFreeBoardRepository;
+import Park.gamewebpage.repository.IUserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FreeBoardCommentApiControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    // 직렬화 역질렬화 를 위해 사용
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    // 서블릿 컨텍스트를 사용하기 위해 사용
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    IFreeBoardCommentRepository iFreeBoardCommentRepository;
+
+    @Autowired
+    IFreeBoardRepository iFreeBoardRepository;
+
+    @Autowired
+    IUserRepository iUserRepository;
+
+    User user;
+
+    /**
+     * mockMvc 객체를 빌더 형식으로
+     * 생성한후 받아오며
+     * 게시판 댓글 저장소를 청소한다.
+     */
+    @BeforeEach
+    public void setMockMvc(){
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+
+        iFreeBoardRepository.deleteAll();
+        iFreeBoardCommentRepository.deleteAll();
+    }
+
+    /**
+     * 테스트에 사용할 계정을 생성해준다.
+     */
+    @BeforeEach
+    void setSecurityContext(){
+        iUserRepository.deleteAll();
+        user = iUserRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test").build());
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities()));
+    }
+
+    /**
+     * 댓글을 달 대상인  자유게시판 글 이다.
+     * @throws Exception
+     */
+    public FreeBoard createFreeBoard() throws Exception{
+        // CreateFreeBoardDTO 객체에 담을 변수
+        final String title = "타이틀1";
+        final String content = "콘텐츠1";
+
+        return iFreeBoardRepository.save(FreeBoard.builder()
+                .title("title")
+                .writerId(user.getUsername())
+                .content("content")
+                .build());
+    }
+
+    @DisplayName("createFreeBoard: 자유게시판 글 추가에 성공하였다.")
+    @Test
+    void createFreeBoardComment() throws Exception{
+        // 자유게시판 하나 생성후 시작
+        FreeBoard freeBoard = createFreeBoard();
+        // given
+        // url 변수 생성
+        // 미리생성된 자유게시판을 대상으로 한다.
+        final String url = "/api/freeBoard/"+freeBoard.getId()+"/freeBoardComment";
+
+        final String comment = "코맨트";
+
+        final CreateFreeBoardCommentDTO createFreeBoardCommentDTO
+                = new CreateFreeBoardCommentDTO(comment,freeBoard);
+
+        final String requestBody
+                = objectMapper.writeValueAsString(createFreeBoardCommentDTO);
+
+        Principal principal = Mockito.mock(Principal.class);
+        Mockito.when(principal.getName()).thenReturn("username");
+
+        // when
+        // 설정한 내용을 바타으로 요청을 전송한다.
+        ResultActions result
+                = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .principal(principal)
+                .content(requestBody));
+
+        // then
+        // HTTP 응답코드가 isCreated 201 상태를 기대한다.
+        result.andExpect(status().isCreated());
+        List<FreeBoardComment> freeBoardCommentList = iFreeBoardCommentRepository.findAll();
+
+        Assertions.assertThat(freeBoardCommentList.size()).isEqualTo(1); // 크기가 1인지 체크
+        Assertions.assertThat(freeBoardCommentList.get(0).getComment()).isEqualTo(comment);
+    }
+
+}
---
+++ gamewebpage/src/test/java/Park/gamewebpage/controller/GameHighScoreApiControllerTest.java
@@ -0,0 +1,98 @@
+package Park.gamewebpage.controller;
+
+import Park.gamewebpage.domain.GameHighScore;
+import Park.gamewebpage.repository.IGameHighScoreRepository;
+import Park.gamewebpage.url.URL;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GameHighScoreApiControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    // 직렬화 역질렬화 를 위해 사용
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    // 서블릿 컨텍스트를 사용하기 위해 사용
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    IGameHighScoreRepository iGameHighScoreRepository;
+
+    /**
+     * mockMvc 객체를 빌더 형식으로
+     * 생성한후 받아오며
+     * 게임 캐릭터 저장소를 청소한다.
+     */
+    @BeforeEach
+    public void setMockMvc(){
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+
+        iGameHighScoreRepository.deleteAll();
+    }
+
+    @Test
+    void getGameCharacterRankPage() throws Exception {
+        //given
+        final String url = URL.GAME_CHARACTER_RANK_API;
+
+        GameHighScore gameCharacter1 =
+                iGameHighScoreRepository.save(
+                        GameHighScore.builder()
+                                .email("1")
+                                .gameCharacterNickname("1")
+                                .highScore(1L)
+                                .lastedTime(LocalDateTime.now())
+                                .build());
+        GameHighScore gameCharacter2 =
+                iGameHighScoreRepository.save(
+                        GameHighScore.builder()
+                                .email("2")
+                                .gameCharacterNickname("2")
+                                .highScore(2L)
+                                .lastedTime(LocalDateTime.now())
+                                .build());
+        GameHighScore gameCharacter3 =
+                iGameHighScoreRepository.save(
+                        GameHighScore.builder()
+                                .email("3")
+                                .gameCharacterNickname("3")
+                                .highScore(3L)
+                                .lastedTime(LocalDateTime.now())
+                                .build());
+
+        //when
+        final ResultActions resultActions
+                = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].email").value("3"))
+                .andExpect(jsonPath("$[1].email").value("2"))
+                .andExpect(jsonPath("$[2].email").value("1"));
+
+    }
+}
